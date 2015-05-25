badd9040c31a35b26e69d589cd94fcf304f3725f
Mark Dennis <markndennis@hotmail.com>
2015-05-25 22:54:38 +0000

    remove old db files

--- examinee/admin.py
+++ examinee/admin.py
@@ -9,9 +9,9 @@ class ExamineeAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name','dob','intended','invigilator','pk')
     
     
-class ExamTypeAdmin(admin.ModelAdmin):
-    #fields = [ 'last_name','first_name', 'email']
-    list_display = ('exam_type','pk')    
+# class ExamTypeAdmin(admin.ModelAdmin):
+#     #fields = [ 'last_name','first_name', 'email']
+#     list_display = ('exam_type','pk')    
     
 admin.site.register(Examinee, ExamineeAdmin)
 
